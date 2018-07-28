33637731f63b78419b912e537bc3af3ad5de223b
Piotr Murach <pmurach@gmail.com>
2018-07-28 16:52:43 +0200

    Add examples

---
+++ examples/basic.rb
@@ -0,0 +1,12 @@
+require_relative '../lib/tty-pie_chart'
+
+data = [
+  { name: 'BTC', value: 5977, color: :bright_yellow },
+  { name: 'BCH', value: 3045, color: :bright_green },
+  { name: 'LTC', value: 2030, color: :bright_magenta },
+  { name: 'ETH', value: 2350, color: :bright_cyan }
+]
+
+pie = TTY::PieChart.new(data, radius: 10)
+
+puts pie.draw
---
+++ examples/fill.rb
@@ -0,0 +1,12 @@
+require_relative '../lib/tty-pie_chart'
+
+data = [
+  { name: 'BTC', value: 5977, color: :bright_yellow, fill: '*' },
+  { name: 'BCH', value: 3045, color: :bright_green, fill: 'x' },
+  { name: 'LTC', value: 2030, color: :bright_magenta, fill: '@' },
+  { name: 'ETH', value: 2350, color: :bright_cyan, fill: '+' }
+]
+
+pie = TTY::PieChart.new(data, radius: 5)
+
+puts pie.draw
